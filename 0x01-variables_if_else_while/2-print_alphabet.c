52b8fb479fc1859b5478bf110a37d6a69afef18b
Ayodele Akinyode <AkinyodeAyodeleCareer@gmail.com>
2023-02-17 12:08:25 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+
 /**
 * main - Prints the alphabet.
 *
-*Return: Always 0 (Success)
+* Return: Always 0 (Success)
 */
 int main(void)
 {
@@ -10,9 +11,9 @@ int main(void)
 	int i;
 
 	for (i = 0; i < 26; i++)
-{
-	putchar(alph[i]);
-}
+	{
+		putchar(alp[i]);
+	}
 	putchar('\n');
 	return (0);
 }
