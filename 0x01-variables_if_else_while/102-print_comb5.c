92e495bdc77c6952be6012b9af6d1a6a68fbfa37
Ayodele Akinyode <AkinyodeAyodeleCareer@gmail.com>
2023-02-17 15:39:00 -0800

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int p, q;
+
+for (p = '0'; p < '98' ; p++)
+{
+
+for (q = p + 1; q <= '99' ; q++)
+{
+putchar((p / 10) + '0');
+putchar((p % 10) + '0');
+putchar(' ');
+putchar((q / 10) + '0');
+putchar((q % 10) + '0');
+putchar(' ');
+if (p == '7' && q == '8')
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
