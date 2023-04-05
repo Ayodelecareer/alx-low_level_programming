0106a6f4b2729104a5605192993c8cc7b10e189d
Ayodelecareer <123329621+Ayodelecareer@users.noreply.github.com>
2023-04-05 05:10:43 +0100

    Linked List Updates

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint_safe - frees a listint_t list (can free lists with a loop)
+ * @h: pointer to head of singly linked list
+ *
+ * Return: the size of the list that was free’d
+ */
+size_t free_listint_safe(listint_t **h)
+{
+listint_t *bck = *h, *bck2;
+unsigned int count = 0;
+
+if (lst == 0 || h == 0)
+return (0);
+
+bck = *h;
+while (bck != 0)
+{
+bck2 = bck;
+bck = bck->next;
+count++;
+
+free(bck2);
+
+if (bck2 <= bck)
+break;
+}
+
+*h = 0;
+return (count);
+}
